8a8dfea0072e6142b4702fd3149f427886570a49
Drahomír Dlabaja <dr.dlabaja@gmail.com>
2018-11-07 09:28:46 +0100

    fast dct

--- include/dct.h
+++ include/dct.h
@@ -9,6 +9,18 @@
 
 using namespace std;
 
+constexpr array<float, 64> init_coefs() {
+  array<float, 64> c {};
+  for(uint64_t u = 0; u < 8; ++u) {
+    for(uint64_t x = 0; x < 8; ++x) {
+      c[u*8+x] = cos(((2 * x + 1) * u * M_PI ) / 16) * sqrt(0.25) * (u == 0 ? (1 / sqrt(2)) : 1);
+    }
+  }
+  return c;
+}
+
+constexpr array<float, 64> coefs = init_coefs();
+
 template <uint8_t D>
 struct fdct {
   template <typename IF, typename OF>
@@ -28,18 +40,10 @@ template <>
 struct fdct<1> {
   template <typename IF, typename OF>
   fdct(IF &&input, OF &&output) {
-    for (uint8_t x = 0; x < 8; x++) {
-      output(0) += input(x);
-    }
-
-    output(0) /= sqrt(2);
-    output(0) *= sqrt(0.25);
-
-    for (uint8_t u = 1; u < 8; u++) {
+    for (uint8_t u = 0; u < 8; u++) {
       for (uint8_t x = 0; x < 8; x++) {
-        output(u) += input(x) * cos(((2 * x + 1) * u * M_PI ) / 16);
+        output(u) += input(x) * coefs[u*8+x];
       }
-      output(u) *= sqrt(0.25);
     }
   }
 };
@@ -63,13 +67,9 @@ struct idct<1> {
   template <typename IF, typename OF>
   idct(IF &&input, OF &&output) {
     for (uint8_t x = 0; x < 8; x++) {
-      output(x) = input(0) / sqrt(2);
-
-      for (uint8_t u = 1; u < 8; u++) {
-        output(x) += input(u) * cos(((2 * x + 1) * u * M_PI) / 16);
+      for (uint8_t u = 0; u < 8; u++) {
+        output(x) += input(u) * coefs[u*8+x];
       }
-
-      output(x) *= sqrt(0.25);
     }
   }
 };
