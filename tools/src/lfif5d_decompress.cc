20d474b43a738b3d475f12f410b89ed1faabd77a
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2019-06-23 15:48:53 +0200

    a

--- tools/src/lfif5d_decompress.cc
+++ tools/src/lfif5d_decompress.cc
@@ -114,7 +114,7 @@ int main(int argc, char *argv[]) {
           }
           else {
             for (size_t col = 0; col < ppm.width; col++) {
-              uint16_t *data_ptr = reinterpret_cast<const uint16_t *>(rgb_data.data());
+              const uint16_t *data_ptr = reinterpret_cast<const uint16_t *>(rgb_data.data());
               ppm_row[col].r = data_ptr[(((flushed_frames_count * views_count + view) * ppm.height + row) * ppm.width + col) * 3 + 0];
               ppm_row[col].g = data_ptr[(((flushed_frames_count * views_count + view) * ppm.height + row) * ppm.width + col) * 3 + 1];
               ppm_row[col].b = data_ptr[(((flushed_frames_count * views_count + view) * ppm.height + row) * ppm.width + col) * 3 + 2];
