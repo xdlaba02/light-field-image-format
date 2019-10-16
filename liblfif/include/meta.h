b9639b925208f5a6f708bb080138d70e65255434
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2019-10-16 15:03:30 +0200

    rename

--- liblfif/include/meta.h
+++ liblfif/include/meta.h
@@ -1,13 +1,13 @@
 /**
-* @file constpow.h
+* @file meta.h
 * @author Drahomír Dlabaja (xdlaba02)
 * @date 12. 5. 2019
 * @copyright 2019 Drahomír Dlabaja
-* @brief Compile-time exponentiation.
+* @brief Compile-time stuff.
 */
 
-#ifndef CONSTPOW_H
-#define CONSTPOW_H
+#ifndef META_H
+#define META_H
 
 /**
 * @brief  Function which performs integer exponentiation.
@@ -19,5 +19,17 @@ template<class T>
 inline constexpr T constpow(const T base, const unsigned exponent) {
     return (exponent == 0) ? 1 : (base * constpow(base, exponent - 1));
 }
+
+/*
+template<size_t D, typename F>
+void meta_for(size_t range[D], F &&callback) {
+  for (size_t i { 0 }; i < range[D - 1]; i++) {
+    auto new_callback = [&](size_t indices[D - 1]) {
+      size_t new_indices[D] =
+      callback()
+    }
+  }
+}
+*/
 
 #endif
